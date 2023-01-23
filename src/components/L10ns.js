ad0f9b1a03dfae118abd425bb05685b739394f16
Kevin Foster <kevin.foster.uk@gmail.com>
2023-01-23 18:39:38 +0800

    Refactor L10ns using suggestion from Jonas

--- src/components/L10ns.js
+++ src/components/L10ns.js
@@ -1,10 +1,5 @@
-const L10ns = ({ prop, lang, l10ns }) => {
-    let defaultLang = 'en';
-    let localValue = l10ns && l10ns[lang] && l10ns[lang][prop] ? l10ns[lang][prop] : null;
-    if (localValue === null) {
-        localValue = l10ns && l10ns[defaultLang] && l10ns[defaultLang][prop] ? l10ns[defaultLang][prop] : null;
-    }
-    return localValue;
-}
+const default_language = 'en';
+
+const L10ns = ({ prop, lang, l10ns }) => l10ns?.[lang]?.[prop] || l10ns?.[default_language]?.[prop]
 
 export default L10ns
