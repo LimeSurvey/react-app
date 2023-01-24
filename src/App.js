2cbfe3183a537d3ed7a40518a2b7b7bc9361e622
Kevin Foster <kevin.foster.uk@gmail.com>
2023-01-24 18:35:57 +0800

    Refactor router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import ThemeProvider from 'react-bootstrap/ThemeProvider'
-import Router from 'Router'
+import router from 'router'
 import { RouterProvider } from 'react-router-dom'
+import ThemeProvider from 'react-bootstrap/ThemeProvider'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 
 import { queryClient, persistOptions } from 'query'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'themes/default.scss'
 
@@ -13,7 +14,7 @@ function App() {
     return (
         <ThemeProvider breakpoints={['lg', 'xl']}>
             <PersistQueryClientProvider client={queryClient} persistOptions={persistOptions}>
-                <RouterProvider router={Router} />
+                <RouterProvider router={router} />
                 <ReactQueryDevtools initialIsOpen={false} />
             </PersistQueryClientProvider>
         </ThemeProvider>
